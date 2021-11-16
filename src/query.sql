f49adfe9fcda31c53483b5c56ea6b1b5bc59967f
cuongtien00 <cuonghiep000@gmail.com>
2021-11-16 14:36:26 +0700

    sua lai ban ve

--- src/query.sql
+++ src/query.sql
@@ -1,12 +1,12 @@
 create database Case_study_Module3;
 use Case_study_Module3;
 create table role(
-                     id int(10) not null primary key ,
+                     id int(10) not null auto_increment primary key ,
                      name varchar(20)
 );
 create table user(
-                     id int(10) not null primary key ,
-                     fullname varchar(30),
+                     id int(10) not null auto_increment primary key ,
+                     fullName varchar(50),
                      introduction varchar(50),
                      username varchar(30),
                      password varchar(30)
@@ -20,20 +20,39 @@ create table role_user(
                               ON DELETE CASCADE
 );
 create table post(
-                     id int(10) not null primary key ,
-                     title varchar(20),
-                     content varchar(50),
+                     id int(10) not null auto_increment primary key ,
+                     title varchar(500),
+                     content text,
+                     image text,
                      user_id int(10),
                      foreign key (user_id) references user(id)
                          ON DELETE CASCADE
 );
 create table comment(
-                        id int(10) not null primary key ,
-                        content varchar(50),
+                        id int(10) not null auto_increment primary key ,
+                        content text,
                         post_id int(10),
                         user_id int(10),
                         foreign key (post_id) references post(id)
                             ON DELETE CASCADE,
                         foreign key (user_id)references user(id)
                             ON DELETE CASCADE
+);
+create table relationship(
+                             receipt_id int(10) ,
+                             send_id int(10),
+                             status int(10),
+                             foreign key (receipt_id)references user(id)
+                                 ON DELETE CASCADE,
+                             foreign key (send_id)references user(id)
+                                 ON DELETE CASCADE
+);
+create table likePost(
+                         id int(10) auto_increment not null primary key ,
+                         post_id int(10),
+                         user_id int(10),
+                         foreign key (post_id)references post(id)
+                             ON DELETE CASCADE,
+                         foreign key (user_id)references user(id)
+                             ON DELETE CASCADE
 );
